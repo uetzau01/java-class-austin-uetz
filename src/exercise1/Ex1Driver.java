19b45d8f21d09daf5b1fa5f4e66b1ec5f94e72b9
uetzau01 <uetzau01@luther.edu>
2017-10-25 23:25:38 -0500

    Updating/finishing exercise 1

--- src/exercise1/Ex1Driver.java
+++ src/exercise1/Ex1Driver.java
@@ -2,6 +2,7 @@ package exercise1;
 
 import java.util.Scanner;
 import java.util.Random;
+import java.util.Arrays;
 
 /**
  * @author Austin Uetz
@@ -31,6 +32,7 @@ public class Ex1Driver {
    * TODO: Task 1: Print "Hello World" to the default system output
    */
   public static void task1() {
+    System.out.println("---Task 1---");
     System.out.println("Hello World");
   }
   /**
@@ -38,9 +40,10 @@ public class Ex1Driver {
    */
   public static void task2() {
     System.out.println();
+    System.out.println("---Task 2---");
     for (int i=1; i<=100; ++i) {
         if (i%15 == 0) {
-            System.out.println("Huh?");
+            System.out.println("huh");
         } 
         else if (i%5 == 0) {
             System.out.println("bar");
@@ -57,19 +60,21 @@ public class Ex1Driver {
    */
   public static void task3() {
     System.out.println();
+    System.out.println("---Task 3---");      
     int sum = 0;
     for (int i=0; i<=100; ++i) {
         if (i % 7 == 0) {
             sum = sum + i;
         }
     }
-    System.out.println(sum);
+    System.out.println("The sum of all multiples of 7 between 1 and 100 is " + sum);
   }
   /**
    * TODO: Task 4: Calculate and print sum and product of all numbers between 1 and 20 (inclusive)
    */
   public static void task4() {
     System.out.println();
+    System.out.println("---Task 4---");      
     int sum = 0;
     long product = 1;
     for (int i=1; i<=20; ++i) {
@@ -84,10 +89,10 @@ public class Ex1Driver {
    */
   public static void task5() {
     System.out.println();
+    System.out.println("---Task 5---");      
     Scanner keyboard = new Scanner(System.in);
     System.out.printf("Enter your full name%n");
     String fullName = keyboard.nextLine();
-    System.out.printf("Your name is %s%n", fullName);
     String[] arrayName = fullName.split(" ");
     System.out.print("Your initials are: ");
     for (int i = 0; i < arrayName.length; ++i) {
@@ -100,12 +105,13 @@ public class Ex1Driver {
    */
   public static void task6() {
     System.out.println();
+    System.out.println("---Task 6---");      
     int[] array = new int[10];
     for (int i = 0; i < array.length; ++i) {
         array[i] = i * i * i;
     }
     for (int i=0; i < array.length; ++i) {
-        System.out.println("Element at index " + i + ": " + array[i]);
+        System.out.println(array[i]);
     }
   }
   /**
@@ -113,6 +119,7 @@ public class Ex1Driver {
    */
   public static void task7() {
     System.out.println();
+    System.out.println("---Task 7---");      
     Random rnd = new Random();
     rnd.setSeed(252);
     int[] randArray = new int[100];
@@ -126,6 +133,8 @@ public class Ex1Driver {
     for (int i=0; i < randArray.length; ++i) {
         if (randArray[i] == userNum) {
             found=true;
+            System.out.println("Your number is in the array");
+            break;
         }
     }
     if (found==true) {
@@ -139,12 +148,13 @@ public class Ex1Driver {
    * @return 
    */
   public static int task8(int[] numbers) {
-      System.out.println();
+    System.out.println();
+    System.out.println("---Task 8---");      
       int sum = 0;
       for (int i = 0; i < numbers.length; ++i) {
           sum = sum + numbers[i];
       }
-      System.out.println(sum);
+      System.out.println("The sum of all items in " + Arrays.toString(numbers) + " is " + sum);
       return sum;
   }
   /**
@@ -152,18 +162,20 @@ public class Ex1Driver {
    * @return 
    */
   public static boolean task9() {
+    System.out.println();
+    System.out.println("---Task 9---");      
     Scanner keyboard = new Scanner(System.in);
     System.out.println("Enter a word");
     String word = keyboard.nextLine();
     boolean ordered = true;
     for (int i=0; i < word.length()-1; ++i) {
         if ((int)word.charAt(i) > (int)word.charAt(i+1)) {
-            System.out.println("The word is not in alphabetical order");
+            System.out.println("Letters of " + word + " are NOT in order");
             ordered = false;
             return ordered;
         }
         
-        } System.out.println("The word is in alphabetical order");
+        } System.out.println("Letters of " + word + " ARE in order");
     return ordered;
   }
  
@@ -171,7 +183,10 @@ public class Ex1Driver {
    * TODO: Task 10: Implement sieve of Eratosthenes and print all prime numbers between 1 and 100
    */
   public static void task10() {
+    System.out.println();
+    System.out.println("---Task 10---");      
     int[] array = new int[100];
+    System.out.println("Prime numbers between 1 and 100");
     for (int i=2; i < array.length; ++i) {
         if (array[i] == 0) {
             System.out.print(i + " ");
