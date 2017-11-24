373900306325587961c9417a7988c342a72a4e88
uetzau01 <uetzau01@luther.edu>
2017-11-23 18:35:16 -0600

    finishing exercise 9

--- src/exercise9/Ex9Converter.java
+++ src/exercise9/Ex9Converter.java
@@ -6,26 +6,26 @@ package exercise9;
  */
 public class Ex9Converter {
   public Double c2f(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return ((valueFrom * 9 / 5) + 32);
   }
 
   public Double c2k(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return valueFrom + 273.15;
   }
 
   public Double f2c(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return ((valueFrom - 32) * 5 / 9);
   }
 
   public Double f2k(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return ((valueFrom - 32) * 5 / 9) + 273.15;
   }
 
   public Double k2c(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return valueFrom - 273.15;
   }
 
   public Double k2f(Double valueFrom) {
-    throw new UnsupportedOperationException();
+    return ((valueFrom - 273.15) * 9 / 5) + 32;
   }
 }
---
+++ src/notes/Greeter.java
@@ -0,0 +1,11 @@
+package notes;
+
+/**
+ *
+ * @author uetzau01
+ */
+public class Greeter {
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+}
