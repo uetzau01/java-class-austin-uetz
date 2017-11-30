4dab63935700c06f7c541ca369a09a67c07bc4df
uetzau01 <uetzau01@luther.edu>
2017-11-30 14:47:25 -0600

    finishing exercise11

--- src/exercise11/Ex11Driver.java
+++ src/exercise11/Ex11Driver.java
@@ -2,7 +2,8 @@ package exercise11;
 
 /**
  * Exercise 11
- * @author yasiro01
+ * @author Austin Uetz
+ * @assignment Exercise 11
  */
 public class Ex11Driver {
   public static void main(String[] args) {
--- src/exercise11/Ex11Logic.java
+++ src/exercise11/Ex11Logic.java
@@ -4,7 +4,7 @@ import java.util.Collection;
 
 /**
  * Exercise 11 logic
- * @author yasiro01
+ * @author Austin Uetz
  */
 public class Ex11Logic {
   
@@ -15,6 +15,10 @@ public class Ex11Logic {
    * @return length of the parameter's string representation
    */
   // TODO: Implement method strLen
+    public <T> int strLen(T item) {
+        String s = item.toString();
+        return s.length();
+    }
   
   /**
    * Task 2
@@ -25,6 +29,17 @@ public class Ex11Logic {
    * @return the greater of three parameters
    */
   // TODO: Implement method greater
+    public <T extends Comparable> T greater(T item1, T item2, T item3) {
+        if (item1.compareTo(item2) >= 0) {
+            if (item1.compareTo(item3) >= 0) {
+                return item1;
+            }
+        } else if (item2.compareTo(item1) >= 0) {
+            if (item2.compareTo(item3) >= 0) {
+                return item2;
+            }
+        } return item3;
+    }
   
   /**
    * Task 3
@@ -33,6 +48,11 @@ public class Ex11Logic {
    * print a list of items
    */
   // TODO: Implement method printList
+    public <T> void printList(T ... list) {
+        for (T item: list) {
+            System.out.print(item + " ");
+        }
+    }
   
   /**
    * Task 4
@@ -41,6 +61,22 @@ public class Ex11Logic {
    * @return the smallest item in the collection
    */
   // TODO: Implement method findMinItem
+    public <T extends Comparable> T findMinItem(Collection<T> coll) {
+        T minimum = null;
+        int count = 0;
+        
+        for (T item: coll) {
+            count = count + 1;
+            if (count == 1) {
+                minimum = item;
+            }
+            if (item.compareTo(minimum) < 0) {
+                minimum = item;
+            }
+        
+        }
+    return minimum;
+}
 }
 
   /**
@@ -51,3 +87,36 @@ public class Ex11Logic {
    * Implement the constructor, getters, setters, and toString
    */
   // TODO: Implement class Foo
+class Foo<T, U> {
+    private T fuzz;
+    private U buzz;
+    
+    public Foo(T fuzz, U buzz) {
+        this.fuzz = fuzz;
+        this.buzz = buzz;
+    }
+
+    public T getFuzz() {
+        return fuzz;
+    }
+
+    public U getBuzz() {
+        return buzz;
+    }
+
+    public void setFuzz(T fuzz) {
+        this.fuzz = fuzz;
+    }
+
+    public void setBuzz(U buzz) {
+        this.buzz = buzz;
+    }
+
+    @Override
+    public String toString() {
+        return "Foo{" + "fuzz=" + fuzz + ", buzz=" + buzz + '}';
+    }
+    
+    
+    
+}
